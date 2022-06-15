135b52e3d9177f0e4b73fbc3540261c33e24a779
minh <rnrnck78@naver.com>
2022-06-15 15:27:53 +0900

    first commit

---
+++ KeyShortCut.java
@@ -0,0 +1,10 @@
+package Problem;
+
+public class KeyShortCut {
+    public static void main(String[] args) {
+        //sout : System.out.println();
+        //ctrl + alt + s : 설정
+        //
+
+    }
+}
