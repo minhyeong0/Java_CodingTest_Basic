0aec93979aebe78d3a4e08ba75f62c8300f96c2f
minh <rnrnck78@naver.com>
2022-06-20 14:55:14 +0900

    Problem4

---
+++ Problem4/main.java
@@ -0,0 +1,51 @@
+package Problem.Problem4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        String[] str = new String[n];
+
+        for(int i=0; i<n; i++){
+            str[i] = s.next();
+        }
+
+        for(String x : solution1(n,str)){
+            System.out.println(x);
+        }
+    }
+
+    private static ArrayList<String> solution1(int n, String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
+        for(String x : str){
+            String tmp = new StringBuilder(x).reverse().toString();
+            answer.add(tmp);
+        }
+        return  answer;
+    }
+
+    private static ArrayList<String> solution2(int n, String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
+        for(String x : str){
+            char[] s = x.toCharArray();
+            int lt = 0;
+            int rt = x.length()-1;
+            while (lt<rt){
+                char tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = s[tmp];
+                lt++;
+                rt--;
+            }
+            String tmp = String.valueOf(s);
+            answer.add(tmp);
+
+        }
+        return  answer;
+    }
+}
