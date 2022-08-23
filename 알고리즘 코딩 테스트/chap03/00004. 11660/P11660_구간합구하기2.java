f9969d1a9c7145e80edd981dad81d5389989f19f
minh <rnrnck78@naver.com>
2022-08-23 11:00:49 +0900

    11660 complete

--- 알고리즘 코딩 테스트/chap03/00004. 11660/P11660_구간합구하기2.java
+++ 알고리즘 코딩 테스트/chap03/00004. 11660/P11660_구간합구하기2.java
@@ -6,15 +6,16 @@ import java.util.StringTokenizer;
 
 
 class P11660_구간합구하기2{
+	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		
 		int[][] A = new int[N+1][N+1];
 		
+		
+		//배열의 값 저장하기
 		for(int i=1; i<=N; i++){
 			st = new StringTokenizer(br.readLine());
 			for(int j=1; j<=N; j++){
@@ -22,12 +23,25 @@ class P11660_구간합구하기2{
 			}
 		}
 		
-		int D[][] = new int[N+1][N+1];
-		
+		int[][] D = new int[N+1][N+1];
+		//합배열 구하기
 		for(int i=1; i<=N; i++){
 			for(int j=1; j<=N; j++){
-				
+				D[i][j] = D[i-1][j] + D[i][j-1] - D[i-1][j-1] + A[i][j];
 			}
 		}
+		
+		for(int i=0; i<M; i++){
+			st = new StringTokenizer(br.readLine());
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+			
+			int res = D[x2][y2] - D[x2][y1-1] - D[x1-1][y2] + D[x1-1][y1-1];
+			System.out.println(res);
+		}
+		
+		
 	}
 }
